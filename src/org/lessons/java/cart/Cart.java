03df21c09d87ae71c554818d9c11dc74e2bc5611
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-28 00:59:17 +0100

    added Cart class with main method to collect product details via input

---
+++ src/org/lessons/java/cart/Cart.java
@@ -0,0 +1,24 @@
+package org.lessons.java.cart;
+
+import java.util.Scanner;
+
+public class Cart {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci i dati del prodotto richiesti e premi invio...");
+        System.out.println("Che prodotto vuoi aggiungere? 1 - Smartphone, 2 - Televisore, 3 - Cuffie");
+        int typeOfProduct = input.nextInt();
+        input.nextLine();
+
+        System.out.println("Nome prodotto: ");
+        String name = input.nextLine();
+        System.out.println("Marchio prodotto: ");
+        String brand = input.nextLine();
+        System.out.println("Prezzo prodotto: ");
+        double price = input.nextDouble();
+        System.out.println("IVA prodotto: ");
+        int vat = input.nextInt();
+
+
+    }
+}
