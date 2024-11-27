709f1217a45cbecb8dbeef6d0003ae0e021f239a
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-11-28 00:17:37 +0100

    added instance variable, constructor and added getters and setters

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,5 +1,50 @@
 package org.lessons.java.shop;
 
 public class Product {
+    private int code;
+    private String name;
+    private String brand;
+    private double price;
+    private int vat;
+    
+    //Constructor
+    public Product(int code, String name, String brand, double price, int vat){
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.vat = vat;
+    }
+
+    //Getters
+    public int getCode(){
+        return code;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public int getVat(){
+        return vat;
+    }
+
+    //Setters
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public void setVat(int vat){
+        this.vat = vat;
+    }
 
 }
