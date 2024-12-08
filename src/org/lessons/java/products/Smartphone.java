2d939c6a837b52f94925596663755b176dedb9e9
Francesco-Msc <moscafrancesco1998@gmail.com>
2024-12-08 16:25:43 +0100

    Added Screen composition to Smartphone class

--- src/org/lessons/java/products/Smartphone.java
+++ src/org/lessons/java/products/Smartphone.java
@@ -1,18 +1,21 @@
 package org.lessons.java.products;
 
 import org.lessons.java.compositions.Processor;
+import org.lessons.java.compositions.Screen;
 import org.lessons.java.shop.Product;
 
 public class Smartphone extends Product {
     private int imei;
     private int memory;
     private Processor processor;
+    private Screen screen;
 
-    public Smartphone(int code, String name, String brand, double price, int vat, int imei, int memory, Processor processor){
+    public Smartphone(int code, String name, String brand, double price, int vat, int imei, int memory, Processor processor, Screen screen){
         super(code, name, brand, price, vat);
         this.imei = imei;
         this.memory = memory;
         this.processor = processor;
+        this.screen = screen;
     }
 
     public Processor getProcessor(){
@@ -36,5 +39,6 @@ public class Smartphone extends Product {
     public void phoneDetails(){
         System.out.println("Nome: " + getName() + ", Marchio: " + getBrand() + ", Prezzo: " + getPrice() + ", IVA: " + getVat() + ", IMEI: " + imei + ", Memoria: " + memory);
         System.out.println("Processore: " + processor.getName() + ", Core: " + processor.getCores());
+        System.out.println("Schermo:" + screen.getName() + ", refresh rate: " + screen.getHz() + " Hz");
     }
 }
